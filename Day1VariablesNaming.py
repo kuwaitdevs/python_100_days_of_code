41e94da7cbda2cf999ca5e033bf6228e042a1437
Anas Najaa <spidernet12@gmail.com>
2024-11-23 09:53:16 +0300

    Day 1

---
+++ Day1VariablesNaming.py
@@ -0,0 +1,10 @@
+# the naming is not clear
+n = "Angela"
+l = len(n)
+print(l)
+
+username = "Angela"
+
+# you can use numbers but not at the start of your variable
+
+# don't use reserved words such ad input and len for your variable names
